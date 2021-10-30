c807e6b43c1af9ab76103fb6cb56bac66ee87d3c
Michal <m.kotecki96@o2.pl>
2021-10-31 01:28:30 +0200

    first try to make input boxes

--- keeper_class.py
+++ keeper_class.py
@@ -1,13 +1,9 @@
 import json
-import tkinter as tk
-
-from pynput import keyboard
 
 
 class PasswordKeeper:
     def __init__(self, source, username, password):
         self.__passes = {}
-        self.length = len(self.__passes)
         self.source = source
         self.username = username
         self.password = password
@@ -25,21 +21,17 @@ class PasswordKeeper:
     def add_password(self):
         self.__passes[self.source] = {self.username: self.password}
 
+    def addition(self):
+        self.add_source()
+        self.add_username()
+        self.add_password()
 
 
 password_keeper = PasswordKeeper(input("Enter source app or website: "),
                                  input("Enter username or e-mail address: "),
                                  input("Enter password: "))
 
-
-def addition():
-    password_keeper.add_source()
-    password_keeper.add_username()
-    password_keeper.add_password()
-
-
-addition()
-
+password_keeper.addition()
 
 with open('passes.json', 'a+') as file:
     json.dump(password_keeper.passes, file, indent=4, separators=(',', ': '))
